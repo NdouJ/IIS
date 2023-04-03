7303bacbb2b9a9082b89358855ce15d52d652c55
NdouJ <jelena.ndou@gmail.com>
2023-04-04 01:32:57 +0200

    xml xsd validation no servixe or ui

--- DogBreeds/Controllers/HomeController.cs
+++ DogBreeds/Controllers/HomeController.cs
@@ -12,8 +12,8 @@ namespace DogBreeds.Controllers
 {
     public class HomeController : Controller
     {
-     
-    private  DogBreedsServices dogBreedsServices = new DogBreedsServices();
+
+        private DogBreedsServices dogBreedsServices = new DogBreedsServices();
 
         private readonly ILogger<HomeController> _logger;
 
@@ -25,16 +25,16 @@ namespace DogBreeds.Controllers
         public async Task<IActionResult> IndexAsync()
         {
 
-         List<DogBreed>doggos = new List<DogBreed>();    
+            List<DogBreed> doggos = new List<DogBreed>();
 
-         doggos = (List<DogBreed>)await dogBreedsServices.GetDoggosListAsync();
-      
+            doggos = (List<DogBreed>)await dogBreedsServices.GetDoggosListAsync();
+
 
             return View(doggos);
 
         }
 
-        public  IActionResult IndexXsd()
+        public IActionResult IndexXsd()
         {
 
             //static void Main(string[] args)
@@ -61,49 +61,204 @@ namespace DogBreeds.Controllers
 
         }
         [HttpPost]
+
         public IActionResult Upload(IFormFile file)
         {
-            if (file != null && file.Length > 0)
-            {
-                if (Path.GetExtension(file.FileName).ToLower() == ".xml")
-                {
-                    using (var reader = new StreamReader(file.OpenReadStream()))
-                    {
-                        var xml = reader.ReadToEnd();
-                        ViewBag.Xml = xml;
-                        return View("Result");
-                    }
-                }
-                else
-                {
-                    ViewBag.Error = "The selected file is not an XML file.";
-                }
-            }
-            else
+            if (file == null || file.Length == 0)
             {
                 ViewBag.Error = "Please select a file to upload.";
+                return View("Result", "nije uplodan file");
             }
 
-            return View();
+            if (Path.GetExtension(file.FileName).ToLower() != ".xml")
+            {
+                ViewBag.Error = "The selected file is not an XML file.";
+                return View("Result", "file nije .xml");
+            }
+
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                var xml = reader.ReadToEnd();
+                ViewBag.Xml = xml;
+
+                // Load the XSD schema from the file system
+                XmlSchemaSet schemaSet = new XmlSchemaSet();
+                schemaSet.Add(null, @"C:\\IIS\\DogBreeds\\XSDDogBreeds.xsd");
+
+                // Create a reader settings object and assign the schema to it
+                var readerSettings = new XmlReaderSettings();
+                readerSettings.Schemas = schemaSet;
+
+                // Set the validation type and event handler for validation errors
+                readerSettings.ValidationType = ValidationType.Schema;
+                var isValid = true;
+                readerSettings.ValidationEventHandler += (sender, args) =>
+                {
+                    Console.WriteLine($"Validation error: {args.Message}");
+                    isValid = false;
+                };
+
+                // Create an XML reader and validate the document against the schema
+                using (var reader2 = XmlReader.Create(new StringReader(xml), readerSettings))
+                {
+                    while (reader2.Read())
+                    {
+                        // do nothing, just read the whole document
+                    }
+                }
+
+                string r = isValid ? "d" : "n";
+                return View("Result", r);
+            }
         }
 
 
 
+        //    public IActionResult Upload(IFormFile file)
+        //    {
+
+        //        string xmlString;
+        //        if (file != null && file.Length > 0)
+        //        {
+        //            if (Path.GetExtension(file.FileName).ToLower() == ".xml")
+        //            {
+        //                using (var reader = new StreamReader(file.OpenReadStream()))
+        //                {
+        //                    var xml = reader.ReadToEnd();
+        //                    ViewBag.Xml = xml;
+        //                    xmlString = xml.ToString();
+
+        //                    XmlSchemaSet schemaSet = new XmlSchemaSet();
+        //                    schemaSet.Add(null, @"C:\\IIS\\DogBreeds\\XSDDogBreeds.xsd");
+
+        //                    //   return View("Result", xmlString);
 
 
+        //                    // Create a reader settings object and assign the schema to it
+        //                    XmlReaderSettings readerSettings = new XmlReaderSettings();
+        //                    readerSettings.Schemas = schemaSet;
+
+        //                    // Set the validation type and event handler for validation errors
+        //                    readerSettings.ValidationType = ValidationType.Schema;
+        //                    readerSettings.ValidationEventHandler += (sender, args) =>
+        //                    {
+        //                        Console.WriteLine($"Validation error: {args.Message}");
+        //                    };
+
+        //                    // Create an XML reader and validate the document against the schema
+        //                    using (XmlReader reader2 = XmlReader.Create(new StringReader(xml), readerSettings))
+        //                    {
+        //                        try
+        //                        {
+        //                            while (reader2.Read())
+        //                            {
+        //                                // do nothing, just read the whole document
+        //                            }
+        //                            return View("Result", "validan"); // if there are no errors, return true
+        //                        }
+        //                        catch (XmlException)
+        //                        {
+        //                            return View("Result", "nije validan"); ; // if there is an XmlException, return false
+        //                        }
+        //                        catch (XmlSchemaException)
+        //                        {
+        //                            return View("Result", " exc");// if there is an XmlSchemaException, return false
+        //                        }
+        //                    }
+
+
+        //                }
+        //            }
+        //            else
+        //            {
+        //                ViewBag.Error = "The selected file is not an XML file.";
+        //                return View("Result", " neka greska");
+        //            }
+
+        //            /*
+        //                     else
+        //    {
+        //        ViewBag.Error = "The selected file is not an XML file.";
+        //    }
+        //}
+        //else
+        //{
+        //    ViewBag.Error = "Please select a file to upload.";
+        //}
+
+        //             */
+
+        //            /*
+        //             using System.Xml;
+        //using System.Xml.Schema;
+
+        //public static bool ValidateXmlAgainstXsd(string xmlFilePath, string xsdFilePath)
+        //{
+        //    // Load the XML document to be validated
+        //    XmlDocument doc = new XmlDocument();
+        //    doc.Load(xmlFilePath);
+
+        //    // Load the XSD schema from your project
+        //    XmlSchemaSet schemaSet = new XmlSchemaSet();
+        //    schemaSet.Add(null, xsdFilePath);
+
+        //    // Create a reader settings object and assign the schema to it
+        //    XmlReaderSettings readerSettings = new XmlReaderSettings();
+        //    readerSettings.Schemas = schemaSet;
+
+        //    // Set the validation type and event handler for validation errors
+        //    readerSettings.ValidationType = ValidationType.Schema;
+        //    readerSettings.ValidationEventHandler += (sender, args) =>
+        //    {
+        //        Console.WriteLine($"Validation error: {args.Message}");
+        //    };
+
+        //    // Create an XML reader and validate the document against the schema
+        //    using (XmlReader reader = XmlReader.Create(xmlFilePath, readerSettings))
+        //    {
+        //        try
+        //        {
+        //            while (reader.Read())
+        //            {
+        //                // do nothing, just read the whole document
+        //            }
+        //            return true; // if there are no errors, return true
+        //        }
+        //        catch (XmlException)
+        //        {
+        //            return false; // if there is an XmlException, return false
+        //        }
+        //        catch (XmlSchemaException)
+        //        {
+        //            return false; // if there is an XmlSchemaException, return false
+        //        }
+        //    }
+        //}
+
+        //             */
+        //            // return View();
+        //        }
+
+        //        else
+        //        {
+        //            return View("Result", "Ne znam kaj je puklo");
+        //        }
+
+
+        //    }
 
 
 
 
         public IActionResult Privacy()
-        {
-            return View();
-        }
+            {
+                return View();
+            }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+            public IActionResult Error()
+            {
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
         }
     }
-}
